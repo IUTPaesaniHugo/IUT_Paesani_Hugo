519edd903df0d1dc16dd2dc0497c9f3db190b4b7
ClementDevauchelle <112684591+ClementDevauchelle@users.noreply.github.com>
2023-03-15 17:19:06 +0100

    TP3-14.5bis

--- Projet_Paesani_Devauchelle/CSharp/RobotInterface/RobotInterface/PID.cs
+++ Projet_Paesani_Devauchelle/CSharp/RobotInterface/RobotInterface/PID.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace RobotInterface
 {
-    class Class1
+
+    public class PID
     {
+        public float kp;
+        public float ki;
+        public float kd;
+        public float promax;
+        public float intmax;
+        public float dermax;
+        public float consigne;
+        public float erreur;
+        public float commande;
+        public float corrP;
+        public float corrI;
+        public float corrD;
+
+        public PID()
+        {
+        }
     }
 }
